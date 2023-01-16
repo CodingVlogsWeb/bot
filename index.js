1afbef489bc89d90a9ec11fced9f9fe5c9346326
CodingVlogsWeb <101495213+CodingVlogsWeb@users.noreply.github.com>
2023-01-16 13:03:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-const Discord = require("discord.js");
-const CommandHandler = require("wokcommands");
-const client = new Discord.Client({
-  partials: ["MESSAGE", "REACTION"],
+const Discord = require('discord.js');
+const openai = require('openai');
+
+// Set up the Discord client and OpenAI API key
+const client = new Discord.Client();
+openai.apiKey = "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";
+
+// When the bot is ready, log to the console
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
-const welcome = require("./welcome");
-
-const config = require("./config.json");
-
-client.on("ready", () => {
-  console.log("Our Discord bot is online");
-
-  new CommandHandler(client, { commandsDir: "commands" });
-  welcome(client);
+// Listen for messages sent to the bot
+client.on('message', async message => {
+    if (message.content.startsWith("!openai")) {
+        // Use the OpenAI API to generate a response
+        const response = await openai.Completion.create({
+            prompt: message.content,
+            model: "text-davinci-002"
+        });
+        // Send the response back to the Discord channel
+        message.channel.send(response.choices[0].text);
+    }
 });
 
-client.login(config.token);
+// Log in to Discord using the bot token
+client.login("MTA1ODk4NzE1ODQ4MTkzNjM4NA.G81GGO.K4bpgiTTSTjACoaufjBDNdtsiHCgewjRf6SR_g");
